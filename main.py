c2ff558f2afe4cac7ced01e59ad7c325daacf78a
khonlionking <khonlionking@gmail.com>
2023-12-06 00:36:22 +0800

    2023-12-06 00:36:22

--- main.py
+++ main.py
@@ -49,3 +49,4 @@ export class OpenAI extends Chat {
   public async askStream(req: ChatRequest, stream: EventStream) {
     const data: RealReq = {
       messages: req.messages,
+      temperature: 1.0,
