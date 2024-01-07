83dcd625ac1eff7731a53b99d6df622b97550163
khonlionking <khonlionking@gmail.com>
2024-01-08 00:38:29 +0800

    2024-01-08 00:38:29

--- main.py
+++ main.py
@@ -98,3 +98,4 @@ export class OpenAI extends Chat {
 import { AxiosInstance, AxiosRequestConfig, CreateAxiosDefaults } from 'axios';
 import { CreateAxiosProxy } from '../../utils/proxyAgent';
 import es from 'event-stream';
+import { Event, EventStream, parseJSON } from '../../utils';
