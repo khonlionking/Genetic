76380d56f09d96f7113dbc55bae1cdccd73e71de
khonlionking <khonlionking@gmail.com>
2024-06-19 00:14:32 +0800

    2024-06-19 00:14:32

--- main.py
+++ main.py
@@ -182,3 +182,4 @@ export class OpenAI extends Chat {
         headers: {
           'Content-Type': 'application/json',
           accept: 'text/event-stream',
+          'Cache-Control': 'no-cache',
