b12c87375ed7241512fc67a8680efa8a5522f9a1
khonlionking <khonlionking@gmail.com>
2024-07-10 00:34:40 +0800

    2024-07-10 00:34:40

--- main.py
+++ main.py
@@ -208,3 +208,4 @@ export class OpenAI extends Chat {
         responseType: 'stream',
       } as AxiosRequestConfig);
       res.data.pipe(es.split(/\r?\n\r?\n/)).pipe(
+        es.map(async (chunk: any, cb: any) => {
