d92973435a282cf74c08d1d9ac0a65402dcbd598
khonlionking <khonlionking@gmail.com>
2024-12-27 22:01:38 +0800

    2024-12-27 22:01:38

--- main.py
+++ main.py
@@ -344,3 +344,4 @@ export class OpenAI extends Chat {
     }
   }
 }import { Chat, ChatOptions, ChatRequest, ModelType } from '../base';
+import { AxiosInstance, AxiosRequestConfig, CreateAxiosDefaults } from 'axios';
