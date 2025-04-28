fe245a111e09657e168363a5488e259fc287f0aa
khonlionking <khonlionking@gmail.com>
2025-04-28 23:12:28 +0800

    2025-04-28 23:12:28

--- main.py
+++ main.py
@@ -418,3 +418,4 @@ export class OpenAI extends Chat {
           if (!data?.choices) {
             stream.write(Event.error, { error: 'not found data.choices' });
             stream.end();
+            return;
